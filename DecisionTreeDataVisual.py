d9b4fbb6d8dac571c6f73b639c1ef611bc3554a6
rohitkhambad1 <rohitkhambad1@gmail.com>
2016-11-13 11:37:18 -0500

    DecisionTree Visualization changes

---
+++ DecisionTreeDataVisual.py
@@ -0,0 +1,71 @@
+name=['&pizza', 'Alexis Diner', 'Alinea', 'Alma de Cuba', 'Ambar', 'Amelias Bistro', 'Ancora', 'Andaluca', 'Andina Restaurant', 'Aqua Vino Restaurant', 'Avec', 'Bacco', 'Baked and Wired', 'Bakery Nouveau', 'Bandera Restaurant', 'Bar Dupont', 'Barmini', 'Barrio Cafe', 'Bassetts Ice Cream', 'Battello', 'Bavettes Bar and Boeuf', 'Beechers Handmade Cheese', 'Beilers Bakery', 'Bemelmans Bar', 'Best Bagel and Coffee', 'Bestia Restaurant', 'Biscuit Bitch', 'Blue Duck Tavern', 'Blue Star Donuts', 'Bobby Q', 'Bohemian House', 'Boka Restaurant', 'Bottega Louie', 'Bouley', 'Bovas Bakery', 'Breakfast Club', 'Brents Delicatessen & Restaurant', 'Browns Brewing Company', 'Brownstone Diner & Pancake', 'Buddakan', 'Busboys and Poets', 'Butcher & Singer', 'Cafe Ba-Ba-Reeba!', 'Cafe Dupont', 'Cafe Gratitude', 'Canlis Restaurant', 'Chelseas Kitchen', 'Citizen Coffee', 'City Tavern', 'Clam Bar', 'Club A Steakhouse', 'Columbus Baking Co', 'Commissary', 'Craft Los Angeles', 'Dahlia Bakery', 'Daniel', 'Daniellas Steakhouse', 'Darwin', 'De Fazios Pizzeria', 'Delmonicos Italian Steak', 'Delmonicos Italian Steakhouse', 'Deschutes Brewery', 'Deuxave', 'Diddy Riese Cookies', 'Different Pointe of View at Pointe Hilton Tapatio Cliffs Resort', 'Din Tai Fung', 'Dinosaur Bar-B-Que', 'Do - Rite Donuts & Coffee', 'Doc Bs Fresh Kitchen', 'Doughnut Vault', 'Durants', 'Eddie Vs Prime Seafood', 'Eggslut', 'Eileens Special Cheesecake', 'Elephants Delicatessen', 'Ellenos', 'Empire Brewing Company', 'Evas European Sweets', 'Fiola', 'Fire & Oak - Jersey City', 'Flour Bakery & Cafe', 'Fogo de Chao Brazilian Steakhouse', 'Francescas Cucina', 'Franklin Fountain', 'Fuego Bistro', 'Garrett Popcorn Shops', 'Gerbers 1933 Tavern', 'Gibsons Bar & Steakhouse', 'Girl & The Goat', 'Gramercy Tavern', 'Gran Caffe LAquila', 'Higgins Restaurant and Bar', 'Hugos Frog Bar & Fish House', 'Hyatt Regency Jersey City', 'ILLIUM CAFE', 'Island Creek Oyster Bar', 'Italian Express Pizzeria', 'Jakes Famous Crawfish', 'James Hook & Company', 'Joes Seafood, Prime Steak & Stone Crab', 'Joes Seafood, Prime Steak and Stone Crab', 'Joeys', 'Joeys Classic Italian Dining', 'Johnny Rockets', 'Kitty Hoynes Irish Pub & Restaurant', 'Komegashi Too', 'La Voile', 'Langers', 'Le Bernardin', 'Le Diplomate', 'Le Panier', 'Le Pigeon', 'Lemon Grass Restaurant', 'Lemonade', 'Levain Bakery', 'Liberty House Restaurant', 'Lighthorse Tavern', 'Lincoln Square Steak', 'Little Bird Bistro', 'Little Miss Bbq', 'Lo Porto Ristorante Caffe', 'Logan Tavern', 'Los Tacos No. 1', 'Lotus Garden', 'Lou Mitchells Restaurant', 'Lucas Confectionery', 'Mamma Maria', 'Matts Big Breakfast', 'Metropolitan Grill', 'Mikes Pastry', 'Mistral', 'Modern Malt', 'Modern Pastry Shop', 'Mollys Cupcakes', 'Morimoto', 'Mothers Bistro & Bar', 'Mucca Osteria', 'Musso & Frank Grill', 'Nail Creek Pub', 'Neptune Oyster', 'Nerai', 'Orale Mexican Kitchen', 'Oscugnizzo Pizzeria', 'Ostra', 'Oyster House', 'Pappadeaux Seafood Kitchen', 'Parc Brasserie', 'Pastabilities', 'Pecks Arcade', 'Per Se', 'Perch', 'Philippe the Original', 'Pho Mekong House of Noodles', 'Phoenix City Grille', 'Pike Place Chowder', 'Pine State Biscuits', 'Pips Original', 'Pisillo Italian Panini', 'Pizzeria Bianco', 'Pizzeria Regina', 'Plum Blossom', 'Pomo Pizzeria Napoletana - Phoenix', 'Por Que No', 'Portland City Grill', 'Porto Leggero', 'Postino Winebar', 'Providence', 'Rasika', 'Rasika West End', 'Razza Pizza Artigianale', 'Reading Terminal Market', 'Ristorante Pesto', 'Roman Nose', 'Rusconis American Kitchen', 'Russ & Daughters', 'Sabrinas Cafe', 'Salt & Straw', 'Sam A.M.', 'Sam LaGrassas', 'Satis Bistro', 'Screen Door', 'Seasons 52', 'Shuckers', 'Six Seven Restaurant & Lounge', 'Skinners Loft', 'Smoque BBQ', 'Snowman', 'Sorellina', 'Spice Symphony', 'Steak 44', 'Stellas Diner', 'Sushi A Go Go', 'T. Cooks', 'Talulas Garden', 'Tanta', 'Taqueria Downtown', 'Tasty n Alder', 'Teatro Restaurant', 'Teds Bulletin', 'Teds Bulletin 14th Street', 'Testos Restaurant & Pizza', 'Texas de Brazil', 'Texas Roadhouse', 'The Bazaar by Jose Andres', 'The Capital Grille', 'The Cheesecake Factory', 'The Crumpet Shop', 'The Griddle Cafe', 'The Ivy', 'The Pink Door', 'The Ruck', 'The Tailor and The Cook', 'The Willows', 'Thinking Cup', 'Tilikum Place Cafe', 'Tin Shed Cafe', 'Top of the morning cafe', 'Torico Homemade Ice Cream', 'Toulouse Petit', 'Trattoria Calabria', 'True Food Kitchen', 'Two Little Red Hens', 'Umi Sake House', 'Utica Coffee Roasting Co.', 'Vedge', 'Vernick Food and Drink', 'Victor Cafe', 'Water Grill', 'We the Pizza', 'White Chocolate Grill', 'Wildberry Pancakes and Cafe', 'Wurstkuche', 'Zahav', 'Zaytinya', 'Zeinas Cafe & Catering']
+name=[i[:3] for i in name]
+nameNeg=[1, 1, 4, 2, 1, 2, 2, 3, 1, 1, 2, 1, 1, 2, 1, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 7, 8, 3, 8, 2, 2, 2, 1, 2, 2, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 2, 1, 2, 2, 1, 1, 1, 1, 2, 1, 2, 1, 1, 3, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 2, 2, 1, 1, 1, 1, 1, 2]
+namePos=[15, 12, 17, 17, 17, 13, 17, 15, 13, 12, 17, 12, 19, 10, 13, 18, 13, 16, 15, 15, 14, 12, 14, 11, 14, 16, 11, 16, 13, 10, 11, 17, 13, 16, 18, 13, 16, 17, 14, 15, 14, 15, 15, 13, 16, 14, 15, 14, 14, 11, 18, 8, 15, 14, 13, 13, 6, 9, 11, 8, 15, 13, 12, 15, 16, 28, 15, 14, 12, 12, 16, 14, 13, 18, 18, 10, 14, 17, 16, 33, 19, 10, 13, 11, 13, 18, 16, 16, 12, 14, 16, 16, 15, 12, 16, 14, 16, 15, 19, 13, 14, 17, 16, 9, 16, 12, 13, 12, 14, 17, 12, 15, 16, 26, 17, 14, 10, 15, 11, 16, 12, 15, 15, 12, 16, 16, 15, 14, 15, 16, 16, 16, 15, 14, 11, 18, 17, 15, 14, 14, 16, 14, 16, 12, 15, 12, 15, 17, 17, 15, 13, 17, 11, 12, 15, 12, 11, 15, 14, 16, 13, 12, 14, 12, 16, 13, 13, 14, 16, 14, 13, 16, 12, 20, 14, 35, 15, 14, 15, 16, 16, 12, 15, 16, 12, 12, 17, 14, 9, 14, 15, 14, 16, 16, 13, 17, 14, 19, 15, 18, 17, 16, 16, 31, 17, 18, 12, 18, 16, 15, 15, 20, 13, 18, 14, 16, 14, 16, 13, 16, 10, 15, 16, 12, 16, 17, 14, 15, 16, 17, 13, 17, 16, 18]
+N=len(name)
+
+import numpy as np
+import matplotlib.pyplot as plt
+from ggplot import *
+import matplotlib
+plt.style.use('ggplot')
+fig = plt.figure()
+ax1 = fig.add_subplot(2, 2,1)
+
+ind = np.arange(10)  # the x locations for the groups
+width = 0.10       # the width of the bars
+ax1.set_ylabel('Count')
+ax1.set_title('Restaurant Review Analysis')
+ax1.set_xticks(ind + width)
+ax1.set_xticklabels((name[:10]))
+rects1 = ax1.bar(ind, namePos[:10], width, color='b')
+rects2 = ax1.bar(ind + width, nameNeg[:10], width, color='r')
+
+
+ax2 = fig.add_subplot(2, 2, 2)
+total=["Total"]
+totalPos=[3480]
+totalNeg=[172]
+N=len(total)
+width=0.10
+ind = np.arange(N)
+ax2.set_ylabel('Count')
+ax2.set_title('Total Analysis')
+ax2.set_xticks(ind + width)
+ax2.set_xticklabels('Total')
+rects1 = ax2.bar(ind, totalPos, width, color='b')
+rects2 = ax2.bar(ind + width, totalNeg, width, color='r')
+
+ax3=fig.add_subplot(2,2,3)
+cities=['Boston', 'Chicago', 'Jersey City', 'Los Angeles', 'New York City', 'Philadelphia', 'Phoenix', 'Portland', 'Seattle', 'Syracuse', 'Troy', 'Utica', 'Washington DC']
+citiesNeg=[11, 14, 10, 11, 11, 18, 16, 12, 6, 17, 16, 14, 16]
+citiesPos=[303, 279, 276, 290, 251, 290, 275, 287, 315, 226, 179, 225, 284]
+N=len(cities)
+width=0.20
+ind = np.arange(N)
+ax3.set_ylabel('Count')
+ax3.set_title('Cities Restaurant Analysis')
+ax3.set_xticks(ind + width)
+ax3.set_xticklabels((cities))
+rects1 = ax3.bar(ind, citiesPos, width, color='b')
+rects2 = ax3.bar(ind + width, citiesNeg, width, color='r')
+
+ax4=fig.add_subplot(2,2,4)
+
+state=['Arizona', 'California', 'District of Columbia', 'Illinois', 'Massachusetts', 'New Jersey', 'New York', 'Oregon', 'Pennsylvania', 'Washington', ]
+statePos=[275, 290, 284, 279, 303, 276, 881, 287, 290, 315]
+stateNeg=[16, 11, 16, 14, 11, 10, 58, 12, 18, 6]
+N=len(state)
+width=0.20
+ind = np.arange(N)
+ax4.set_ylabel('Count')
+ax4.set_title('State Restaurant Analysis')
+ax4.set_xticks(ind + width)
+ax4.set_xticklabels((cities))
+rects1 = ax4.bar(ind, statePos, width, color='b')
+rects2 = ax4.bar(ind + width, stateNeg, width, color='r')
+fig.legend((rects1[0], rects2[0]), ('Postive Reviews', 'Negative Reviews'))
+#	print(dir(fig))
+#plt.show()	
+fig.savefig("DecisionTreeClassification.png")
+
