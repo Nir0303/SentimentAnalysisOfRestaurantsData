f270b0f6ec102fb0efb2eefbc8dccb679d880172
rohitkhambad1 <rohitkhambad1@gmail.com>
2016-11-13 10:17:20 -0500

    Add files via upload

--- Decision_tree_classifier.py
+++ Decision_tree_classifier.py
@@ -37,5 +37,41 @@ for columns in (raw.strip().split("~") for raw in lines):
 #convert text data to numeric using LabelEncoder
 res = le.fit(list_vec)
 M=le.transform(np.array(list_vec).reshape(-1,1))
-#Predict decision tree results
-dt.predict(M[:3653,:])
+#Predict decision tree results on test data
+o=dt.predict(M[:3653,:])
+
+#Write results to a file for generating graphs
+#fl_write = open("E:\\hackRPI\\SentimentAnalysisOfRestaurantsData-master\\final_restaurant_data.txt","wb")
+"""with open("E:\\hackRPI\\SentimentAnalysisOfRestaurantsData-master\\final_restaurant_data.txt", 'wb') as f:
+        writer = csv.writer(f)
+        with open("E:\\hackRPI\\SentimentAnalysisOfRestaurantsData-master\\test_restaurant_data.txt","r") as csvfile:
+            reader = csv.reader(csvfile, delimiter='~')
+            for row in reader:
+                row[0] = f1.readline() # edit the 8th column
+                writer.writerow(row)
+                row[1] = f1.readline()
+                writer.writerow(row)
+                row[2] = f1.readline()
+                writer.writerow(row)
+                row[3] = f1.readline()
+                writer.writerow(row)
+                row[4] = f1.readline()
+                writer.writerow(row)
+                writer.writerow(dt[0])"""
+        
+
+opfile="E:\\hackRPI\\SentimentAnalysisOfRestaurantsData-master\\final_restaurant_data.txt"
+ipfile="E:\\hackRPI\\SentimentAnalysisOfRestaurantsData-master\\test_restaurant_data.txt"
+opfil=open(opfile,'w')
+ipfil=open(ipfile)
+cnt=0
+for i in ipfil.readlines():
+    x=i.split('~')
+    if o[cnt]=='1':
+        x[5]='negative'
+    else:
+        x[5]='positive'
+    opfil.write('~'.join(x))
+ipfil.close()
+opfil.close()
+lines.close()
